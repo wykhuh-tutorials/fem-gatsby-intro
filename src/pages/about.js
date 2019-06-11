dc7f53f45e80ede2078e1c9f00d530d06859d95f
wykhuh <wykhuh@users.noreply.github.com>
2019-06-11 05:28:02 -0700

    add global styles to Layout and Header components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About me</h1>
     <p>This is my site</p>
     <Link to="/">home</Link>
-  </>
+  </Layout>
 )
