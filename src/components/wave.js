95d6fda427fc3d35e555cfb1bdfb680023c77c72
wykhuh <wykhuh@users.noreply.github.com>
2019-06-11 17:29:39 -0700

    add Wave component to contact mdx file

---
+++ src/components/wave.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+
+const Wave = () => {
+  // set initial value to zero
+  const [waves, setWaves] = useState(0);
+  const label = ` ${waves} ${waves === 1 ? 'wave' : 'waves'}`
+
+  return (
+    <button css={css`
+      background: purple;
+      border: none;
+      color: white;
+      font-size: 1.25rem;
+    `}
+      onClick={() => setWaves(waves + 1)}
+    >
+      {label}
+    </button>
+  )
+};
+
+export default Wave;
